872cc4541eba0115e98b4fed82506004150d586b
syahadae <rasyidsyahada@gmail.com>
2022-09-06 10:29:57 +0700

    complex model

---
+++ lib/model/user.dart
@@ -0,0 +1,28 @@
+class User {
+  final String gender;
+  final String email;
+  final String cell;
+  final String nat;
+  final String phone;
+  final UserName name;
+
+  User({
+    required this.gender,
+    required this.email,
+    required this.phone,
+    required this.cell,
+    required this.nat,
+    required this.name,
+  });
+}
+
+class UserName {
+  final String title;
+  final String first;
+  final String last;
+  UserName({
+    required this.title,
+    required this.first,
+    required this.last,
+  });
+}
--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
-
+import 'dart:math';
+import 'package:latihanhttp/model/user.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -11,10 +12,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  //bisa dilihat disini  deklarasi users, users disini dinamic ya
-  // darimana tau dinamic?
-  // bisa bukak aja datanya, bentuknya adalah list dynamic
-  List<dynamic> users = [];
+  List<User> users = [];
+
+  @override
+  void initState() {}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,30 +29,17 @@ class _HomeScreenState extends State<HomeScreen> {
       body: ListView.builder(
           itemCount: users.length,
           itemBuilder: (context, index) {
-            // liat dua buah deklrasi dibawah ini, ini sebnarnya
-            // dibuat supaya gampang pas benran manggilnya.
-            // contohnya user ini sudah termasuk user index
-            // dan email ini bisa ditemukan di list user di dalam result
-            // dan di dalam usr ini ada email.
-
             final user = users[index];
-            final email = user['email'];
-            final imgUrl = user['picture']['thumbnail'];
+            // print(users.length);
+            // final color =
+            // user.gender == 'male' ? Colors.green : Colors.lightGreen;
+            final email = user.email;
+            final nat = user.nat;
 
-            final firstName = user['name']['first'];
-
-            final lastName = user['name']['last'];
-            final fullName = firstName + ' ' + lastName;
             return ListTile(
-              leading: ClipRRect(
-                borderRadius: BorderRadius.circular(100),
-                child: CircleAvatar(
-                  child: Image.network(imgUrl),
-                  // child: Text('${index + 1}'),
-                ),
-              ),
-              title: Text(fullName),
-              subtitle: Text(email),
+              title: Text(user.name.first),
+              subtitle: Text(user.phone),
+              // tileColor: color,
             );
           }),
       floatingActionButton: FloatingActionButton(
@@ -61,37 +49,42 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   void FetchUser() async {
-    // pertama pastikan dulu fungsinya bisa dipanggil dengan baik,
-    // print('fetch User called');
-
-    // lalu masukkan url dari network yang dituju
     const url = "https://randomuser.me/api/?results=10";
-    // kalau http ini dia gabisa nerima url secara langsung jadi
-    // harus di parse dulu jadi uri
     final uri = (Uri.parse(url));
-
-    // disini kita harus pakai future karena setiap pengambukan api
-    //butuh waktu yang lumayan lama, jadi future adlah pilihan yang tepat
     final response = await http.get(uri);
-
-    //disini kita ingin mengambil body dari data apinya, jadi
     final body = response.body;
-    //coba print dan liat hasilnya
-    // print(body);
-
-    //parses the string and return the resulting json object
-    // mengubah json string jadi array
     final json = jsonDecode(body);
-    // print(json);
+    final results = json['results'] as List<dynamic>;
 
+    final transformed = results.map(
+      (e) {
+        final name = UserName(
+          title: e['name']['title'],
+          first: e['name']['first'],
+          last: e['name']['last'],
+        );
+        return User(
+          gender: e['gender'],
+          email: e['email'],
+          phone: e['phone'],
+          cell: e['cell'],
+          nat: e['nat'],
+          name: name,
+          // title: e['title'],
+          // first: e['first'],
+          // last: e['last']
+        );
+      },
+    ).toList();
+    // print(results);
+
+    // print(transformed);
     setState(() {
-      //kenapa gini, si json result ini bentuknya list dengan tipe data dynamic, jadi kita butuh
-      // jadi kita butuh variable untuk menampungnya, dan kita buatlah ini
-      //   List<dynamic> users = [];
-      // cba aj print  nanti udah muncul list data dari isi result
-      users = json['results'];
+      users = transformed;
     });
+    // print(results);
 
-    print(users);
+    // print(users);
+    // print(transformed);
   }
 }
